97aa6e9a17166c0e20be8f6b3e1a1122cbce3806
Akshay <53929892+badcoderxd@users.noreply.github.com>
2021-04-28 13:03:52 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/home';
 import Artist from './components/artist';
+import ReactGA from 'react-ga';
 
 
 const Routes = () => {
+        
+        useEffect(() => {
+        ReactGA.initialize('G-B4N5H2DTB4');
+         // To Report Page View 
+        ReactGA.pageview(window.location.pathname + window.location.search);
+        }, [])
+        
         return(
             <Switch>
                 <Route path="/" exact component={Home}/>
